45330c47c9d6221bfccf42380b16a026a514279a
Ilan González Carigi <ilancoder00@gmail.com>
2022-08-16 18:06:53 -0500

    SManager Conection

--- Assets/Scripts/Currencies/Managers/ShaigensManager.cs
+++ Assets/Scripts/Currencies/Managers/ShaigensManager.cs
@@ -1,9 +1,14 @@
+using System;
+using Expeditions.Scriptable;
 using UnityEngine;
 
 namespace Currencies.Managers {
     [DisallowMultipleComponent]
     public class ShaigensManager : MonoBehaviour {
-        public uint Shaigens { get; private set; } 
+        public uint Shaigens { get; private set; }
+
+        void OnEnable() => BasicExpedition.OnExpeditionRewards += AddShaigens;
+        void OnDisable() => BasicExpedition.OnExpeditionRewards -= AddShaigens;
 
         void AddShaigens(uint value) {
             Shaigens += value;
